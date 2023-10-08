fbc6842975219814f047d9a713b0ebf8204aa4e8
Kleo <mmttdd16@gmail.com>
2023-10-08 16:24:53 +0300

    Finished Ex54

--- Ex54/Program.cs
+++ Ex54/Program.cs
@@ -1,2 +1,71 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+int rows = WorkWithUser("Введите кол-во строк: ");
+int columns = WorkWithUser("Введите кол-во столбцов: ");
+int[,] array = GetArray(rows, columns);
+PrintArray(array);
+System.Console.WriteLine();
+NewMatrix(array);
+PrintArray(array);
+
+
+int WorkWithUser(string message)
+{
+    System.Console.Write(message);
+    int number = int.Parse(Console.ReadLine()!);
+    return number; 
+}
+
+int[,] GetArray(int row, int column)
+{
+    int[,] result = new int[row, column];
+    Random rnd = new Random();  
+    for(int i = 0; i < row; i++)
+    { 
+        for (int j = 0; j < column; j++)
+        {
+            result[i, j] = rnd.Next(1, 10); 
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] inArray)
+{
+    for(int i = 0; i < inArray.GetLength(0); i++) // GetLength(0) для строчик
+    { 
+        for (int j = 0; j < inArray.GetLength(1); j++) //GetLength(1) для столбца
+        {
+            System.Console.Write($"{inArray[i, j]} "); 
+        }
+        System.Console.WriteLine();
+    }
+}
+
+void NewMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        // Сортируем элементы в строке по убыванию
+        for (int j = 0; j < matrix.GetLength(1) - 1; j++)
+        {
+            for (int k = j + 1; k < matrix.GetLength(1); k++)
+            {
+                if (matrix[i, k] > matrix[i, j])
+                {
+                    // Меняем местами элементы
+                    int temp = matrix[i, j];
+                    matrix[i, j] = matrix[i, k];
+                    matrix[i, k] = temp;
+                }
+            }
+        }
+    }
+}    
